f18fb4f52fb84fc14cb2c98a7fc9231716266db9
SHUBHSAXENA\shubh <shubhsaxena447@gmail.com>
2025-01-31 21:38:37 +0530

    fix: Fixed CORS Errors in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,13 @@
 import express, { json } from "express"
 import faqRouter from "./routes/FAQ.routes.js"
+import cors from "cors"
 
 const app = express()
 
 app.use(json())
+app.use(cors({
+    origin:process.env.CORS_ORIGIN
+}))
 
 app.use("/api",faqRouter)
 
