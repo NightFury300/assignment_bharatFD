a926bdbea20ec94bc11eeaf4f5ba246ea5f73c93
SHUBHSAXENA\shubh <shubhsaxena447@gmail.com>
2025-01-31 18:18:56 +0530

    feat: Added Basic Database connection, and utilities classes and FAQ model

---
+++ src/models/FAQ.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const faqSchema = new mongoose.Schema({
+  question: { 
+    type: String, 
+    required: true 
+    },
+  answer: { 
+    type: String, 
+    required: true 
+    },
+  translations: {
+    question_hi: String,
+    question_bn: String,
+    },
+});
+
+faqSchema.methods.getTranslatedQuestion = (languageCode) => {
+  const translationKey = `question_${languageCode}`;
+  return this.translations[translationKey] || this.question;
+};
+
+export const FAQ = mongoose.model('FAQ', faqSchema);
